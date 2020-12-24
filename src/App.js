5c64ea952d94e99ad0a712d85708e69d58c997ba
yassine-eluharani <eluharani.yassine@gmail.com>
2020-12-24 11:41:20 +0100

    Random stuff in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the blog";
+  const likes = 1;
+  const link = "http://www.google.com"
+
+
   return (
     <div className="App">
      <div className="content">
        <h1>
-         App Component
+         { title }       
        </h1>
+       <p>Likes { likes } times</p>
+         <p>{ 10 }</p>
+         <p>{"Hello world"}</p>
+         <p>{ [1,54,3] }</p>
+         <p>{ Math.random() * 10 }</p>
+         <a href={link}>Google sites</a>
      </div>
     </div>
   );
