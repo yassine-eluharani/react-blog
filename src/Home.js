b8fd4fa1a5466f7c2e53b35606b9572040cf22dd
yassine-eluharani <eluharani.yassine@gmail.com>
2021-01-08 17:44:21 +0100

    Basic Props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,15 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    //let name = 'Mario';
-    const [name , setName] = useState('Mario');
-    const [age , setAge] = useState(25);
-    const handleCick = () =>{
-      setName('Luigi');
-      setAge(30);
-    }
-    
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old </p>
-            <button onClick= {handleCick}>Click me</button>
-            
+           <BlogList blogs={blogs} title="All Blogs" />
         </div>
      );
 }
