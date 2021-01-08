28618d04e06c4158f1e41c34c6fe39fbe01d84f7
yassine-eluharani <eluharani.yassine@gmail.com>
2021-01-08 16:57:21 +0100

    Events

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,15 @@
 const Home = () => {
-    const title = "Welcome to the blog";
-    const likes = 1;
-    const link = "http://www.google.com"
+    const handleCick = (e) =>{
+        console.log('hello' ,e);
+    }
+    const handleClickAgain = (name ,e) => {
+        console.log('hello ' + name + e);
+    }
     return ( 
         <div className="home">
-            <h1>
-            { title }       
-            </h1>
-            <p>Likes { likes } times</p>
-            <p>{ 10 }</p>
-            <p>{"Hello world"}</p>
-            <p>{ [1,54,3] }</p>
-            <p>{ Math.random() * 10 }</p>
-            <a href={link}>Google sites</a>
+            <h2>Homepage</h2>
+            <button onClick= {handleCick}>Click me</button>
+            <button onClick= { (e) => handleClickAgain('Mario',e) }>Click me again</button>
         </div>
      );
 }
