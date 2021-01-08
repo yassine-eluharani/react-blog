139c9bfdf10de708aa98c249a544b13cf2879989
yassine-eluharani <eluharani.yassine@gmail.com>
2021-01-08 19:43:23 +0100

    json server fetch data not working tittle null

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,11 @@
 import { useState , useEffect } from "react";
 import BlogList from "./BlogList";
 
-const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
 
-//   const [name,setName] = useState('Mario');
+const Home = () => {
+  const [blogs, setBlogs] = useState([null]);
+
+  //const [name,setName] = useState('Mario');
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -16,12 +13,19 @@ const Home = () => {
   }
 
   useEffect(() => {
-      console.log('use effect ran ');    
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+          return res.json()
+      })
+      .then((data) => {  
+        console.log(data)      
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
       {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's blogs" /> */}
       {/* <button onClick= {() => setName('Luigi')} >Change Name</button>
       <p>{name}</p> */}
