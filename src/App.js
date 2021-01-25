064133eb93e97400cfd291db246f013ef481458b
yassine-eluharani <eluharani.yassine@gmail.com>
2021-01-25 15:58:46 +0100

    Route Switch Link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router , Route , Switch } from 'react-router-dom';
+import Create from './Create';
+//surround the app component with router so all component of the app have access
 
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-     <div className="content">
-       <Home />
-     </div>
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route exact path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,28 @@
 import { useState , useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPendig, setIsPending] = useState(true);
-  const [error , setError] = useState(null);
+  const { data:blogs , isPending , error } = useFetch("http://localhost:8000/blogs");
 
   //const [name,setName] = useState('Mario');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
 
-  useEffect(() => {
-        setTimeout(() => {
-          fetch('http://localhost:8000/blogs')
-        .then(res => {
-          if(!res.ok){
-            throw Error('Could not fetch the data');
-          }
-            return res.json()
-        })
-        .then((data) => {  
-          console.log(data)      
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch(err => {
-          setError(err.message);
-          setIsPending(false);
-        })
-      },1000)
-  }, []);
+ 
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
-      {isPendig && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+
+
+      {/* {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />} */}
       {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's blogs" /> */}
       {/* <button onClick= {() => setName('Luigi')} >Change Name</button>
       <p>{name}</p> */}
